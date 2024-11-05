adfd383bc0d8b08dcb7982ad56f376b7cee5de43
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-11-05 12:51:12 +0100

    Updated Header.jsx

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -1,7 +1,6 @@
 import '../scss/styles.scss';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container';
 
 const Header = () => {
@@ -12,19 +11,8 @@ const Header = () => {
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id='basic-navbar-nav'>
           <Nav className='ms-auto'>
-            <Nav.Link href='#home'>Home</Nav.Link>
-            <Nav.Link href='#link'>Link</Nav.Link>
-            <NavDropdown title='Dropdown' id='basic-nav-dropdown'>
-              <NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.2'>
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href='#action/3.4'>
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Nav.Link href='/'>Home</Nav.Link>
+            <Nav.Link href='/login'>Log In</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
