48bb6bcfe9cca69387914a49c879f6c6b3cb0bee
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-11-21 17:33:37 +0100

    req param middleware, not used atm

--- backend/src/v1/routes/userRoutes.js
+++ backend/src/v1/routes/userRoutes.js
@@ -1,6 +1,18 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../../controllers/userController');
+const checkId = require('../../controllers/checkId');
+
+// Only specified for this router!
+// This is a param middleware that will run for only:
+// User routes with a parameter called 'id'
+router.param('id', (req, res, next, value) => {
+  console.log(`User id is: ${value}`);
+  next();
+});
+
+// DOES NOT WORK ATM because of random ID's in users.json file
+//router.param('id', checkId);
 
 router
   .route('/')
