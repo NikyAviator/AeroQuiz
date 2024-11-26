60c71a98e03698151510ca3b030302993ab37ab4
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-11-26 17:04:52 +0100

    mongoose connection established

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
+const mongoose = require('mongoose');
 // On top because:
 // Must load first to set up the environment variables
 const dotenv = require('dotenv');
+const app = require('./app');
+
 dotenv.config({ path: './config.env' });
 
-const app = require('./app');
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+// Connect to MongoDB
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+  })
+  .then((connection) => {
+    // REMOVE IN PROD!!!!
+    console.log(connection.connections);
+    console.log('DB connection successful! 🎉');
+  })
+  .catch((err) => {
+    console.error('DB connection failed! 💥', err.message);
+  });
 
 // Start server
 const port = process.env.PORT || 5000;
