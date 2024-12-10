ac7de73e5654b6287064e018a79142efd45bdfa4
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-12-10 15:46:20 +0100

    Added the text to header.jsx

--- frontend/src/Components/Header.jsx
+++ frontend/src/Components/Header.jsx
@@ -2,11 +2,15 @@ import '../scss/Header.scss';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const Header = () => {
   return (
     <div className='header'>
-      <Navbar expand='lg' className='content'>
+      {/* Navbar */}
+      <Navbar expand='lg' className='navbar'>
         <Container>
           <Navbar.Brand href='/' className='text-white'>
             AeroQuiz
@@ -27,6 +31,39 @@ const Header = () => {
           </Navbar.Collapse>
         </Container>
       </Navbar>
+
+      {/* Second Container */}
+      <Container className='hero-container'>
+        <Row className='align-items-center text-center text-md-start'>
+          <Col md={6}>
+            <h1 className='hero-title'>
+              Master Aviation Knowledge with AeroQuiz
+            </h1>
+            <p className='hero-description'>
+              Prepare for your PPL or ATPL exams with expertly crafted questions
+              on Meteorology, Navigation, Principles of Flight, and more.
+            </p>
+          </Col>
+          <Col md={6}>
+            <Button
+              href='/signup'
+              variant='primary'
+              size='lg'
+              className='hero-button'
+            >
+              Join Now
+            </Button>
+            <Button
+              href='/login'
+              variant='secondary'
+              size='lg'
+              className='hero-button'
+            >
+              Sign In
+            </Button>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 };
