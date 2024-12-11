f328adc18bd176504a357a3f08bfd06f48bc44f1
Nikolai Kocev <nikolai_kocev@hotmail.com>
2024-12-11 10:45:40 +0100

    different headers deps on path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,38 @@
-import { useEffect } from 'react';
 import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
 import Home from './Pages/Home';
 import SignUp from './Pages/SignUp';
 import LogIn from './Pages/LogIn';
-import Header from './Components/Header';
+import Header from './Components/Header'; // Full Header
+import SmallHeader from './Components/SmallHeader'; // Small Header
 import Footer from './Components/Footer';
 
 const App = () => {
   const location = useLocation();
 
-  useEffect(() => {
-    // Clear previous background class
-    document.body.className = '';
-
-    // Set background class based on current path
-    if (location.pathname === '/') {
-      document.body.classList.add('background-home');
-    } else if (location.pathname === '/signup') {
-      document.body.classList.add('background-signup');
-    } else if (location.pathname === '/login') {
-      document.body.classList.add('background-login');
-    }
-  }, [location]);
+  // Determine if the current route is the home page
+  const isHomePage = location.pathname === '/';
 
   return (
-    <>
-      <div className='d-flex flex-column min-vh-100'>
-        <Header />
-        <main className='flex-grow-1'>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/signup' element={<SignUp />} />
-            <Route path='/login' element={<LogIn />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
-    </>
+    <div className='d-flex flex-column min-vh-100'>
+      {/* Conditionally render Header or SmallHeader */}
+      {isHomePage ? <Header /> : <SmallHeader />}
+      <main className='flex-grow-1'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<SignUp />} />
+          <Route path='/login' element={<LogIn />} />
+          {/* Add other routes here */}
+        </Routes>
+      </main>
+      <Footer />
+    </div>
   );
 };
 
-const WrappedApp = () => (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
-);
-
-export default WrappedApp;
+export default function WrappedApp() {
+  return (
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+}
