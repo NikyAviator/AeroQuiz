a4e29d76a1c9b51e0debd5eb6d898bce968897d3
Nikolai Kocev <nikolai_kocev@hotmail.com>
2026-05-21 20:04:51 +0200

    started on main func (backend)

--- backend/services/quiz-service/cmd/main.go
+++ backend/services/quiz-service/cmd/main.go
@@ -1,5 +1,15 @@
 package cmd
 
+import "github.com/NikyAviator/AeroQuiz/backend/shared/env"
+
 func main() {
+	// DB Config (loads from env)
+	mongoURI := env.GetString("MONGODB_URI", "mongodb://localhost:27017")
+	dbName := env.GetString("MONGODB_DBNAME", "aeroquiz")
+	port := env.GetString("PORT", "5000")
+	adminEmail := env.GetString("ADMIN_EMAIL", "")
+	apiSharedSecret := env.GetString("API_SHARED_SECRET", "")
+
+	// Mongo connect
 
 }
