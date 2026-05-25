3af0c18950ac8958a41324f49af4bda386c36b01
Nikolai Kocev <nikolai_kocev@hotmail.com>
2026-05-25 15:32:26 +0200

    user struct added to domain

--- backend/services/quiz-service/internal/domain/user.go
+++ backend/services/quiz-service/internal/domain/user.go
@@ -1,1 +1,16 @@
 package domain
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type User struct {
+	ID           bson.ObjectID `json:"id" bson:"_id,omitempty"`
+	Email        string        `json:"email" bson:"email" binding:"required"`
+	UserName     string        `json:"username" bson:"username" binding:"required"`
+	PasswordHash string        `json:"-" bson:"passwordHash" binding:"required"`
+	CreatedAt    time.Time     `json:"createdAt" bson:"createdAt"`
+	UpdatedAt    time.Time     `json:"updatedAt" bson:"updatedAt"`
+}
